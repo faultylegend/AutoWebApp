6f2d5e71f9f959189d8cd016f2a76c2b15fc6afd
faultylegend <48661622+faultylegend@users.noreply.github.com>
2021-06-17 12:40:07 -0700

    New Function for USAGE

--- development_project/auto/views.py
+++ development_project/auto/views.py
@@ -7,19 +7,16 @@ def home(request):
     return render(request, 'auto/home.html', {'title': 'Home'})
 
 def usage(request):
-    time.sleep(1)
+    time.sleep(5)
+    for _ in range(1):
+        pyautogui.click(828,476)
+        pyautogui.click(811,504)
+        time.sleep(0.5)
+        pyautogui.write("New Patient OK")
 
-    position = pyautogui.position()
-    print(position)
-    pyautogui.moveTo(position.x+100,position.y, duration = 0.25)
-    pyautogui.click()
-    time.sleep(1)
-    pyautogui.click(918,40)
-    time.sleep(1)
-    pyautogui.click(1050,40)
-    time.sleep(1)
-    pyautogui.click(800,40)
-    pyautogui.move(0,100, duration = 0.25)
-    for i in range(5):
-        pyautogui.scroll(200)
+        pyautogui.click(886,547)
+        time.sleep(0.5)
+        pyautogui.click(878,621)
+        pyautogui.click(1290,790)
+
     return render(request, 'auto/usage.html')
